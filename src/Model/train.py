04919a8986a6410a37cde96c185a6524ebfb3968
Cody MacDonald <CodyMacDonald.DSML@gmail.com>
2026-03-17 14:46:26 -0400

    Implemneted early stopping - checking validation decrease with patience

--- src/Model/train.py
+++ src/Model/train.py
@@ -2,7 +2,8 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
+import copy
+from datetime import datetime
 
 # Internal Imports
 
@@ -13,8 +14,9 @@ def fit(
         valid_data: DataLoader,
         loss_fn: nn.Module,
         optimizer: torch.optim.Optimizer,
-        epochs: int,
-        device: str
+        device: str,
+        epochs: int = 1000,
+        patience: int = 5
 ):
     """
     Fit the model with given data.
@@ -25,11 +27,16 @@ def fit(
     :param optimizer: The optimizer to be used for training.
     :param epochs: The number of epochs to train for.
     :param device: The device to be used for training (CPU or GPU).
+    :param patience: The number of epochs to wait for improvement before early stopping.
     :return: dictionary containing training and validation loss and accuracy for each epoch. {Epoch: tuple(train_loss, valid_loss, train_accuracy, valid_accuracy)}
     """
     epoch_data = {}
-
+    best_val_loss = float('inf')
+    patience_counter = 0
+    best_weights = None
+    best_epoch = 0
     for epoch in range(epochs):
+        start = datetime.now()
         model.train()
         total_train_loss = 0.0
         train_correct = 0
@@ -73,6 +80,19 @@ def fit(
 
         # Add epoch metrics
         epoch_data[epoch + 1] = (avg_train_loss, avg_val_loss, avg_train_acc, avg_val_acc)
-        print(f'Epoch [{epoch + 1}/{epochs}] Loss [Train | Validation]: {avg_train_loss:.4f} | {avg_val_loss:.4f} Accuracy: {avg_train_acc:.4f} | {avg_val_acc:.4f}')
+        print(f"Epoch [{epoch + 1}/{epochs} <{str(datetime.now()-start).split('.')[0]}><{patience_counter}/{patience}>] Loss [Train | Validation]: {avg_train_loss:.4f} | {avg_val_loss:.4f} Accuracy: {avg_train_acc:.4f} | {avg_val_acc:.4f}")
 
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            best_weights = copy.deepcopy(model.state_dict())
+            best_epoch = epoch + 1
+            patience_counter = 0
+        else:
+            patience_counter += 1
+
+        if patience_counter == patience:
+            print(f"[INFO] Patience reached, best epoch was {best_epoch} with {best_val_loss:.4f} validation loss.")
+            break
+
+    model.load_state_dict(best_weights)
     return epoch_data
