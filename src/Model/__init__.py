608a269d0be501599bda33f5f6416728006b6d8e
Cody MacDonald <codymacdonald.dsml@gmail.com>
2026-03-21 11:26:03 -0400

    persitance func implemented for model weights

--- src/Model/__init__.py
+++ src/Model/__init__.py
@@ -2,3 +2,4 @@ from .baseline_cnn import BaselineModel
 from .mri_resnet import build_resnet
 from .train import fit
 from .plot_loss_acc import plot_loss_acc
+from .persistance import save_weights, load_weights
