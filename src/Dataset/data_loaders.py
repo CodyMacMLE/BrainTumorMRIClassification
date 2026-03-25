efe14b5fe6e92aaabfe32c710a84573a1ebad5f1
Cody MacDonald <codymacdonald.dsml@gmail.com>
2026-03-25 10:15:56 -0400

    Modified to fix errors from sanity in data_loaders

--- src/Dataset/data_loaders.py
+++ src/Dataset/data_loaders.py
@@ -1,20 +1,21 @@
 # External Imports
-import torch
-from torch.utils.data import DataLoader
-
 # Internal Imports
 from Typedef.Patients import Patients
+from torch.utils.data import DataLoader
+
 from .mri_dataset import MriDataset
-from .transforms import get_train_transforms, get_val_transforms
+
 
 def get_dataloaders(
-      train_data: Patients,
-      val_data: Patients,
-      test_data: Patients,
-      batch_size: int = 32
+        train_data: Patients,
+        val_data: Patients,
+        test_data: Patients,
+        batch_size: int = 32,
+        segmentation = False
   ) -> tuple[DataLoader, DataLoader, DataLoader]:
-    train_dataloader = DataLoader(MriDataset(train_data, transform=get_train_transforms()), batch_size=batch_size, shuffle=True, num_workers=4)
-    val_dataloader = DataLoader(MriDataset(val_data, transform=get_val_transforms()), batch_size=batch_size, shuffle=False, num_workers=4)
-    test_dataloader = DataLoader(MriDataset(test_data, transform=get_val_transforms()), batch_size=batch_size, shuffle=False, num_workers=4)
+
+    train_dataloader = DataLoader(MriDataset(train_data, set_type = "train", segmentation=segmentation), batch_size=batch_size, shuffle=True, num_workers=4)
+    val_dataloader = DataLoader(MriDataset(val_data, set_type = "validation", segmentation=segmentation), batch_size=batch_size, shuffle=False, num_workers=4)
+    test_dataloader = DataLoader(MriDataset(test_data, set_type = "test", segmentation=segmentation), batch_size=batch_size, shuffle=False, num_workers=4)
 
     return train_dataloader, val_dataloader, test_dataloader
--- src/Dataset/transforms.py
+++ src/Dataset/transforms.py
@@ -1,6 +1,62 @@
+import torch
 import torchvision
-from torchvision import transforms
+from torchvision import tv_tensors
+from torchvision.transforms import v2 as transforms
 
+
+"""
+First two transform classes are used for segmentation U-net
+"""
+class Transforms(torch.nn.Module):
+    def __init__(self, pixel_transforms = False):
+        super().__init__()
+        self.transform_pixels = pixel_transforms
+
+        self.spatial_transformers = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomRotation(degrees=360),
+        ])
+
+        if self.transform_pixels:
+            self.pixel_transformers = PixelTransforms()
+
+        self.final_transformers = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    def forward(self, image, mask = None):
+        if mask is not None:
+            mask = tv_tensors.Mask(mask)
+            image, mask = self.spatial_transformers(image, mask)
+        else:
+            image = self.spatial_transformers(image)
+
+        if self.transform_pixels:
+            image = self.pixel_transformers(image)
+
+        image = self.final_transformers(image)
+
+        return image, mask
+
+
+class PixelTransforms(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.transforms = transforms.Compose([
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0, hue=0),
+        ])
+
+    def forward(self, image):
+        image = self.transforms(image)
+        return image
+
+
+"""
+Below are used in the resnet and baseline cnn models
+"""
 def get_train_transforms() -> torchvision.transforms.Compose:
     """
     Augmentation transforms: Random rotations, flips and brightness/contrast jitter
